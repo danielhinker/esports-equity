10ea17450afa6ddca7c3ec09d89975003a5b0181
danielhinker <danhinker@gmail.com>
2020-04-13 21:50:54 -0700

    update db

--- eeweb/settings.py
+++ eeweb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-Online = False
+Online = True
 
 ALLOWED_HOSTS = ['eeweb-test.us-east-1.elasticbeanstalk.com', 'www.esportsequity.com', 'esportsequity.com',
                  'prototype.esportsequity.com', '*']
