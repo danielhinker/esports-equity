f3b6074b28d04885895d667535e43d01dfc7c0ed
danielhinker <danhinker@gmail.com>
2020-04-13 21:22:27 -0700

    Fix syntax error

--- eeweb/settings.py
+++ eeweb/settings.py
@@ -125,7 +125,7 @@ DATE_INPUT_FORMATS = ('%m-%d-%Y',)
 
 import dj_database_url
 # db_from_env = dj_database_url.config()
-DATABASES = { 'default': dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 # else:
 #     DATABASES = {
